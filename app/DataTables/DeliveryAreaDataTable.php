cfb7a5e1686d2284358731f87764078cf28c6554
WATARU-OHKURA <wataruokura17@gmail.com>
2024-09-04 22:35:30 +0800

    save

--- app/DataTables/DeliveryAreaDataTable.php
+++ app/DataTables/DeliveryAreaDataTable.php
@@ -60,7 +60,7 @@ class DeliveryAreaDataTable extends DataTable
                     ->columns($this->getColumns())
                     ->minifiedAjax()
                     //->dom('Bfrtip')
-                    ->orderBy(1)
+                    ->orderBy(0)
                     ->selectStyleSingle()
                     ->buttons([
                         Button::make('excel'),
--- app/Http/Controllers/Frontend/DashboardController.php
+++ app/Http/Controllers/Frontend/DashboardController.php
@@ -3,12 +3,56 @@
 namespace App\Http\Controllers\Frontend;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Frontend\AddressCreateRequest;
+use App\Http\Requests\Frontend\AddressUpdateRequest;
+use App\Models\Address;
+use App\Models\DeliveryArea;
 use Illuminate\Contracts\View\View;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
 {
-    function index() : View {
-        return view('frontend.dashboard.index');
+    function index(): View
+    {
+        $deliveryAreas = DeliveryArea::where('status', 1)->get();
+        $userAddresses = Address::where('user_id', Auth::user()->id)->get();
+        return view('frontend.dashboard.index', compact('deliveryAreas', 'userAddresses'));
+    }
+
+    function createAddress(AddressCreateRequest $request)
+    {
+        $address = new Address();
+        $address->user_id = Auth::user()->id;
+        $address->delivery_area_id = $request->area;
+        $address->first_name = $request->first_name;
+        $address->last_name = $request->last_name;
+        $address->phone = $request->phone;
+        $address->email = $request->email;
+        $address->address = $request->address;
+        $address->type = $request->type;
+        $address->save();
+
+        toastr()->success('Created Successfully!');
+
+        return to_route('dashboard');
+    }
+
+    function updateAddress(AddressUpdateRequest $request, string $id)
+    {
+        $address = Address::findOrFail($id);
+        $address->user_id = Auth::user()->id;
+        $address->delivery_area_id = $request->area;
+        $address->first_name = $request->first_name;
+        $address->last_name = $request->last_name;
+        $address->phone = $request->phone;
+        $address->email = $request->email;
+        $address->address = $request->address;
+        $address->type = $request->type;
+        $address->save();
+
+        toastr()->success('Updated Successfully!');
+
+        return to_route('dashboard');
     }
 }
