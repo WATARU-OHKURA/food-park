cc7e3d83c057ccd9e45266e0c89cc8585308e812
WATARU-OHKURA <wataruokura17@gmail.com>
2024-09-08 10:35:36 +0800

    mail save

--- _ide_helper.php
+++ _ide_helper.php
@@ -19579,6 +19579,34 @@ namespace Flasher\Toastr\Laravel\Facade {
             }
     }
 
+namespace Srmklive\PayPal\Facades {
+            /**
+     * 
+     *
+     */        class PayPal {
+                    /**
+         * Get specific PayPal API provider object to use.
+         *
+         * @throws Exception
+         * @return \Srmklive\PayPal\Services\PayPal 
+         * @static 
+         */        public static function getProvider()
+        {
+                        return \Srmklive\PayPal\PayPalFacadeAccessor::getProvider();
+        }
+                    /**
+         * Set PayPal API Client to use.
+         *
+         * @throws \Exception
+         * @return \Srmklive\PayPal\Services\PayPal 
+         * @static 
+         */        public static function setProvider()
+        {
+                        return \Srmklive\PayPal\PayPalFacadeAccessor::setProvider();
+        }
+            }
+    }
+
 namespace Yajra\DataTables\Facades {
             /**
      * 
@@ -23596,6 +23624,7 @@ namespace  {
             class Livewire extends \Livewire\Livewire {}
             class Flasher extends \Flasher\Laravel\Facade\Flasher {}
             class Toastr extends \Flasher\Toastr\Laravel\Facade\Toastr {}
+            class PayPal extends \Srmklive\PayPal\Facades\PayPal {}
             class DataTables extends \Yajra\DataTables\Facades\DataTables {}
     }
 
--- app/Services/OrderService.php
+++ app/Services/OrderService.php
@@ -43,6 +43,13 @@ class OrderService
                 $orderItem->product_option = json_encode($product->options->product_options);
                 $orderItem->save();
             }
+            
+            /** Putting the Order Id in session */
+            session()->put('order_id', $order->id);
+
+            /** Putting the grand total amount in session */
+            session()->put('grand_total', $order->grand_total);
+
 
             return true;
         } catch (\Exception $e) {
