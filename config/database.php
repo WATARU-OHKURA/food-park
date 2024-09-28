6ca0ae290eb99096461c5ab99c3b61f24692aba8
WATARU-OHKURA <wataruokura17@gmail.com>
2024-09-28 18:13:20 +0800

    Configure MySQL for Heroku deployment

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$JAWSDB_URL = env('JAWSDB_URL');
+
+if ($JAWSDB_URL) {
+    $JAWSDB = parse_url($JAWSDB_URL);
+}
+
+
 return [
 
     /*
@@ -42,11 +49,20 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '3306'),
+            // 'database' => env('DB_DATABASE', 'laravel'),
+            // 'username' => env('DB_USERNAME', 'root'),
+            // 'password' => env('DB_PASSWORD', ''),
+
+            'host' => isset($JAWSDB['host']) ? $JAWSDB['host'] : env('DB_HOST', '127.0.0.1'),
+            'port' => isset($JAWSDB['port']) ? $JAWSDB['port'] : env('DB_PORT', '3306'),
+            'database' => isset($JAWSDB['path']) ? ltrim($JAWSDB['path'], '/') : env('DB_DATABASE', 'forge'),
+            'username' => isset($JAWSDB['user']) ? $JAWSDB['user'] : env('DB_USERNAME', 'forge'),
+            'password' => isset($JAWSDB['pass']) ? $JAWSDB['pass'] : env('DB_PASSWORD', ''),
+            
+
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
