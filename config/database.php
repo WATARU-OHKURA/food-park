40a2c03d1c1fe197c23bf6223c8cce9f260226d5
WATARU-OHKURA <wataruokura17@gmail.com>
2024-09-28 18:00:41 +0800

    s

--- config/database.php
+++ config/database.php
@@ -2,14 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$DATABASE_URL = env('JAWSDB_URL') ?: env('CLEARDB_DATABASE_URL') ?: env('DATABASE_URL');
-
-
-
-if ($DATABASE_URL) {
-    $DATABASE_URL = parse_url($DATABASE_URL);
-}
-
 return [
 
     /*
@@ -50,13 +42,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DB_URL'),
-
-            'host' => isset($DATABASE_URL['host']) ? $DATABASE_URL['host'] : env('DB_HOST', '127.0.0.1'),
-            'port' => isset($DATABASE_URL['port']) ? $DATABASE_URL['port'] : env('DB_PORT', '3306'),
-            'database' => isset($DATABASE_URL['path']) ? ltrim($DATABASE_URL['path'], '/') : env('DB_DATABASE', 'laravel'),
-            'username' => isset($DATABASE_URL['user']) ? $DATABASE_URL['user'] : env('DB_USERNAME', 'root'),
-            'password' => isset($DATABASE_URL['pass']) ? $DATABASE_URL['pass'] : env('DB_PASSWORD', ''),
-
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'laravel'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
