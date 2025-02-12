e8a402dd4b749438b84fb62b4d90140eff9ca0d1
abc-u <114818210+abc-u@users.noreply.github.com>
2025-02-12 15:29:56 +0800

    create map page

---
+++ resources/views/frontend/map/index.blade.php
@@ -0,0 +1,12 @@
+@extends('frontend.layouts.master')
+
+@section('content')
+    <section class="fp__blog_page fp__blog2 mt_120 xs_mt_65 mb_100 xs_mb_70">
+        <div class="container">
+            <h1>Hello Map Page</h1>
+        </div>
+    </section>
+    <!--=============================
+                            BLOG PAGE END
+                        ==============================-->
+@endsection
